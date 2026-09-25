a05348e3333f3f80bcb5392a6b5abde3021d09dc
Migeu53 <miguel.moreno@aluno.senai.br>
2026-09-25 10:40:31 -0300

    Delete src/main directory

--- src/main/java/br/com/amigo/biblioteca/repository/LoanRepository.java
+++
@@ -1,5 +0,0 @@
-package br.com.amigo.biblioteca.repository;
-import br.com.amigo.biblioteca.model.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface LoanRepository extends JpaRepository<Loan,Long>{ List<Loan> findByUserIdOrderByIdDesc(Long userId); }
